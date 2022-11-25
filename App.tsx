f5cb428476a4cc1e46811c2303c2d4f2f3361a71
makarovsergei2020 <50143566+makarovsergei2020@users.noreply.github.com>
2022-11-25 16:18:12 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './style.css';
+
+function App() {
+  return <div>ddad</div>;
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-q5vgma11
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-wzqwrq)
